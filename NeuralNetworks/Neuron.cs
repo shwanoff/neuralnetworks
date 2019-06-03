f2a362478f3fe19fb440602a939a8e7e1f59f277
Вадим Шванов <adm.shwan@gmail.com>
2019-06-03 21:04:56 +0300

    Реализация нейрона

---
+++ NeuralNetworks/Neuron.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetworks
+{
+    public class Neuron
+    {
+        public List<double> Weights { get; }
+        public NeuronType NeuronType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Normal)
+        {
+            NeuronType = type;
+            Weights = new List<double>();
+
+            for(int i = 0; i < inputCount; i++)
+            {
+                Weights.Add(1);
+            }
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for(int i = 0; i < inputs.Count; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            Output = Sigmoid(sum);
+            return Output;
+        }
+
+        private double Sigmoid(double x)
+        {
+            var result = 1.0 / (1.0 + Math.Pow(Math.E, -x));
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+    }
+}
---
+++ NeuralNetworks/NeuronType.cs
@@ -0,0 +1,9 @@
+﻿namespace NeuralNetworks
+{
+    public enum NeuronType
+    {
+        Input = 0,
+        Normal = 1,
+        Output = 2
+    }
+}
