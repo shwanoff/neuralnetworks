414da4e3a8a27ba5c107a1bb1dd32569f67aa407
Вадим Шванов <adm.shwan@gmail.com>
2019-06-05 21:26:19 +0300

    Реализованы методы нормализации и масштабирования. Изменен интерфейс метода learn

--- NeuralNetworks/NeuralNetwork.cs
+++ NeuralNetworks/NeuralNetwork.cs
@@ -37,19 +37,95 @@ namespace NeuralNetworks
             }
         }
 
-        public double Learn(List<Tuple<double, double[]>> dataset, int epoch)
+        public double Learn(double[] expected, double[,] inputs, int epoch)
         {
             var error = 0.0;
-
-            for(int i = 0; i < epoch; i++)
+            for (int i = 0; i < epoch; i++)
             {
-                foreach(var data in dataset)
+                for (int j = 0; j < expected.Length; j++)
                 {
-                    error += Backpropagation(data.Item1, data.Item2);
+                    var output = expected[j];
+                    var input = GetRow(inputs, j);
+
+                    error += Backpropagation(output, input);
                 }
             }
 
             var result = error / epoch;
+            return result;
+        }
+
+        public static double[] GetRow(double[,] matrix, int row)
+        {
+            var columns = matrix.GetLength(1);
+            var array = new double[columns];
+            for (int i = 0; i < columns; ++i)
+                array[i] = matrix[row, i];
+            return array;
+        }
+
+        private double[,] Scalling(double[,] inputs)
+        {
+            var result = new double[inputs.GetLength(0), inputs.GetLength(1)];
+
+            for(int column = 0; column < inputs.GetLength(1); column++)
+            {
+                var min = inputs[0, column];
+                var max = inputs[0, column];
+
+                for(int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    var item = inputs[row, column];
+
+                    if(item < min)
+                    {
+                        min = item;
+                    }
+
+                    if(item > max)
+                    {
+                        max = item;
+                    }
+                }
+
+                var divider = max - min;
+                for (int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    result[row, column] = (inputs[row, column] - min) / divider;
+                }
+            }
+
+            return result;
+        }
+
+        private double[,] Normalization(double[,] inputs)
+        {
+            var result = new double[inputs.GetLength(0), inputs.GetLength(1)];
+
+            for (int column = 0; column < inputs.GetLength(1); column++)
+            {
+                // Среднее значение сигнала нейрона.
+                var sum = 0.0;
+                for (int row = 0; row < inputs.GetLength(0); row++)
+                {
+                    sum += inputs[row, column];
+                }
+                var average = sum / inputs.GetLength(0);
+
+                // Стандартное квадратичное отклонение нейрона.
+                var error = 0.0;
+                for (int row = 0; row < inputs.GetLength(0); row++)
+                {
+                    error += Math.Pow((inputs[row, column] - average), 2);
+                }
+                var standardError = Math.Sqrt(error / inputs.GetLength(0));
+
+                for (int row = 0; row < inputs.GetLength(0); row++)
+                {
+                    result[row, column] = (inputs[row, column] - average) / standardError;
+                }
+            }
+
             return result;
         }
 
--- NeuralNetworksTests/NeuralNetworkTests.cs
+++ NeuralNetworksTests/NeuralNetworkTests.cs
@@ -14,7 +14,8 @@ namespace NeuralNetworks.Tests
         [TestMethod()]
         public void FeedForwardTest()
         {
-            var dataset = new List<Tuple<double, double[]>>
+            var outputs = new double[] { 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 };
+            var inputs = new double[,]
             {
                 // Результат - Пациент болен - 1
                 //             Пациент Здоров - 0
@@ -23,40 +24,41 @@ namespace NeuralNetworks.Tests
                 // Хороший возраст A
                 // Курит S
                 // Правильно питается F
-                //                                             T  A  S  F
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 0, 0, 1, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 1, 1 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 0, 1, 1, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 1, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 0, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 1, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 1, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 1, 1, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 1, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 1, 1 })
+                //T  A  S  F
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 1, 1 },
+                { 0, 1, 0, 0 },
+                { 0, 1, 0, 1 },
+                { 0, 1, 1, 0 },
+                { 0, 1, 1, 1 },
+                { 1, 0, 0, 0 },
+                { 1, 0, 0, 1 },
+                { 1, 0, 1, 0 },
+                { 1, 0, 1, 1 },
+                { 1, 1, 0, 0 },
+                { 1, 1, 0, 1 },
+                { 1, 1, 1, 0 },
+                { 1, 1, 1, 1 }
             };
 
             var topology = new Topology(4, 1, 0.1, 2);
             var neuralNetwork = new NeuralNetwork(topology);
-            var difference = neuralNetwork.Learn(dataset, 100000);
+            var difference = neuralNetwork.Learn(outputs, inputs, 10000);
 
             var results = new List<double>();
-            foreach(var data in dataset)
+            for(int i = 0; i < outputs.Length; i++)
             {
-                var res = neuralNetwork.FeedForward(data.Item2).Output;
+                var row = NeuralNetwork.GetRow(inputs, i);
+                var res = neuralNetwork.FeedForward(row).Output;
                 results.Add(res);
             }
 
             for(int i = 0; i < results.Count; i++)
             {
-                var expected = Math.Round(dataset[i].Item1, 3);
-                var actual = Math.Round(results[i], 3);
+                var expected = Math.Round(outputs[i], 2);
+                var actual = Math.Round(results[i], 2);
                 Assert.AreEqual(expected, actual);
             }
         }
